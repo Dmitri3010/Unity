5c52093a201d5dc3a4119659b13c00c996214f1e
Dmitri <31704410+Dmitri3010@users.noreply.github.com>
2018-10-13 08:52:10 +0300

    laba8

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeColor : MonoBehaviour, IPointerClickHandler {
+
+
+
+    float red, green, blue;
+  
+    void Start() { }
+
+    void Update() { }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+        red = Random.Range(.0f, 1.0f);
+        green = Random.Range(.0f, 1.0f);
+        blue = Random.Range(.0f, 1.0f);
+        Color newcolor = new Color(red, green, blue);
+        int forse = 200;
+    
+        gameObject.GetComponent<Renderer>().material.color = newcolor;
+
+        Vector3 target = eventData.pointerPressRaycast.worldPosition;
+       
+         Vector3 collid = Camera.main.transform.position;
+        Vector3  distance = target-collid;
+        distance = distance.normalized;
+         collid = distance * forse;
+        gameObject.GetComponent<Rigidbody>().AddForceAtPosition(collid, target);
+
+
+    }
+
+}
